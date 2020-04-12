8da676b6e92f30153d8ee36023cb17f8baf8a112
Kazakov <kazakov.kazakov.alexey@gmail.com>
2020-04-12 09:51:23 +0300

    add objects structure and update readmegen

--- README.md
+++ README.md
@@ -6,6 +6,18 @@
 * [Функции](functions.md)
 * [Всплытие](hoisting.md)
 * [Циклы](loops.md)
+* [Объекты](objects.md)
 * [Область видимости](scope.md)
 * [Типы данных](types.md)
 * [Переменные](variables.md)
+
+# TODO (ссылки, которые никуда не ведут)
+
+* this.md *(из файла functions.md)*
+* clean_functions.md *(из файла functions.md)*
+* hof.md *(из файла functions.md)*
+* arrays.md *(из файла loops.md)*
+* strict_mode.md *(из файла scope.md)*
+* template_strings.md *(из файла types.md)*
+* classes.md *(из файла types.md)*
+
--- readmeGen.js
+++ readmeGen.js
@@ -3,50 +3,92 @@ const fs = require("fs");
 
 const README_FILENAME = "README.md";
 
-const readFileP = file => new Promise((resolve, reject) => {
-	fs.readFile(file, (err, data) => {
-		if (err) {
-			reject();
-			return;
-		}
-		resolve(data.toString());
-	})
-});
+const readFileP = (file) =>
+	new Promise((resolve, reject) => {
+		fs.readFile(file, (err, data) => {
+			if (err) {
+				reject();
+				return;
+			}
+			resolve(data.toString());
+		});
+	});
 
-const getReadme = data => `# Список файлов
+const linkRegex = /\[([^\]]*)\]\(([^)]*\.md)\)/giu;
 
-${data.map(({title, filename}) => `* [${title}](${filename})`).join('\n')}`;
+const getReadmeTODO = (unusedLinks) => `# TODO (ссылки, которые никуда не ведут)
+
+${unusedLinks.map(({filename, from}) => `* ${filename} *(из файла ${from})*`).join('\n')}
+
+`;
+
+const getReadmeTOC = (data) => `# Список файлов
+
+${data.map(({ title, filename }) => `* [${title}](${filename})`).join("\n")}
+
+`;
 
 glob(__dirname + "/*.md", {}, async (err, files) => {
 	if (err) {
 		return;
 	}
 	try {
-		const promises = files.map(async file => {
-			const filename = file.split('/').reverse()[0];
-			if (filename === README_FILENAME) {
+		const fileNames = files.reduce((acc, path) => {
+			acc[path] = path.split("/").reverse()[0];
+			return acc;
+		}, {});
+		const fileNamesArray = Object.values(fileNames);
+		const promises = files.map(async (file) => {
+			const currentFileName = fileNames[file];
+			if (currentFileName === README_FILENAME) {
 				return false;
 			}
 			const data = await readFileP(file);
 			const title = data
 				.split("\n")
-				.find(line => line.slice(0, 2) === '# ')
+				.find((line) => line.slice(0, 2) === "# ")
 				.slice(2)
 				.trim();
+			const unusedLinks = [];
+			{
+				let result;
+				while ((result = linkRegex.exec(data)) !== null) {
+					if (result[0] === "[Назад](README.md)") {
+						continue;
+					}
+					const [, name, filename] = result;
+					if (fileNamesArray.includes(filename)) {
+						continue;
+					}
+					unusedLinks.push({
+						filename,
+						from: currentFileName
+					})
+				}
+			}
 			return {
 				title,
-				filename
+				filename: currentFileName,
+				unusedLinks,
 			};
 		});
-		const data = await Promise.all(promises);
-		const readme = getReadme(data.filter(item => item));
-		fs.writeFile(README_FILENAME, readme, (err) => {
+		const data = (await Promise.all(promises)).filter((item) => item);
+		const readmeTOC = getReadmeTOC(data);
+		const unusedLinks = data.reduce((acc, item) => {
+			item.unusedLinks.forEach(link => {
+				if (!acc.some((accItem) => accItem.filename === link.filename)) {
+					acc.push(link)
+				}
+			})
+			return acc;
+		}, []);
+		const readmeTODO = getReadmeTODO(unusedLinks);
+		fs.writeFile(README_FILENAME, `${readmeTOC}${readmeTODO}`, (err) => {
 			if (err) {
 				console.log(err);
 			}
-		})
-	}
-	catch (e) {
-		console.log(e)
+		});
+	} catch (e) {
+		console.log(e);
 	}
 });
