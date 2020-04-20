00cf49a143a92df17cfd6029de97fab3e3f937f5
Kazakov <kazakov.kazakov.alexey@gmail.com>
2020-04-20 15:39:48 +0300

    Update genParts.js

--- readmeGenFiles/genParts.js
+++ readmeGenFiles/genParts.js
@@ -29,7 +29,7 @@ const getReadmeTOC = (data) => `# Список файлов
 ${data
 	.map(
 		({ title, filename, definition, allTitles }, index) =>
-			`## ${index + 1}. [${title}](${filename})<br />
+			`## [${index + 1}. ${title}](${filename})<br />
 ${allTitles
 	.map(({ text, link }) => `[${text}](${filename}#${link})`)
 	.join(" • ")}
