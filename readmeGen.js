9176aca9fcdfabd05097165fb6cb47d1334329f7
Алексей Лауд <kazakov.kazakov.alexey@gmail.com>
2020-10-29 18:56:21 +0300

    prepare

--- readmeGen.js
+++ readmeGen.js
@@ -1,31 +1,22 @@
 const glob = require("glob");
-const fs = require("fs");
 
 const getReadmeText = require("./readmeGenFiles/genParts");
 const proceedFilesList = require("./readmeGenFiles/proceedFilesList");
 const proceedOneFile = require("./readmeGenFiles/proceedOneFile");
-const reduceUnusedLinks = require('./readmeGenFiles/reduceUnusedLinks');
-const {
-	readFileP,
-	writeFileP,
-	getFileNameFromPath,
-} = require("./readmeGenFiles/utils");
-const {
-	README_FILENAME,
-	README_LINK_TEXT,
-	linkRegex,
-} = require("./readmeGenFiles/constants");
+const reduceUnusedLinks = require("./readmeGenFiles/reduceUnusedLinks");
+const { writeFileP } = require("./readmeGenFiles/utils");
+const { README_FILENAME } = require("./readmeGenFiles/constants");
 
 glob("*.md", {}, async (err, filesRaw) => {
-	if (err) {
-		console.error(err);
-		throw err;
-	}
+  if (err) {
+    console.error(err);
+    throw err;
+  }
 
-	const files = await proceedFilesList(filesRaw);
-	const promises = files.map(proceedOneFile({ files }));
-	const data = (await Promise.all(promises)).filter((item) => item);
-	const unusedLinks = reduceUnusedLinks(data);
+  const files = await proceedFilesList(filesRaw);
+  const promises = files.map(proceedOneFile({ files }));
+  const data = (await Promise.all(promises)).filter((item) => item);
+  const unusedLinks = reduceUnusedLinks(data);
 
-	await writeFileP(README_FILENAME, getReadmeText(data, unusedLinks));
+  await writeFileP(README_FILENAME, getReadmeText(data, unusedLinks));
 });
